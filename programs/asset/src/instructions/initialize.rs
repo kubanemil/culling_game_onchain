e9053a5b08c7d35f0c592cd86f63f09db22d554f
kubanemil <emil3toktobekov@gmail.com>
2024-09-10 12:14:57 +0600

    asset init() ix

--- programs/asset/src/instructions/initialize.rs
+++ programs/asset/src/instructions/initialize.rs
@@ -1,9 +1,30 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token_interface::{TokenInterface, Mint};
+use crate::state::AuthVault;
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
 
-pub fn handler(ctx: Context<Initialize>) -> Result<()> {
-    msg!("Greetings from: {:?}", ctx.program_id);
-    Ok(())
+    #[account(init, seeds=[b"emilToken", signer.key.as_ref()], payer=signer, bump, mint::decimals=6, mint::authority=vault)]
+    pub mint: InterfaceAccount<'info, Mint>,
+
+    #[account(init, payer=signer, seeds=[b"authVault"], bump, space=AuthVault::INIT_SPACE)]
+    pub vault: Account<'info, AuthVault>,
+
+    pub token_program: Interface<'info, TokenInterface>,
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> Initialize<'info> {
+    pub fn init(&mut self, bumps: InitializeBumps) -> Result<()> {
+        self.vault.set_inner(
+            AuthVault {
+                bump: bumps.vault,
+                mint_bump: bumps.mint,
+            }
+        );
+        Ok(())
+    }
 }
