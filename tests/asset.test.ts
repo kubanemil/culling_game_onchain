9d8cd151a824adae1fbbfcf7a2ac162d31c0e0d1
kubanemil <emil3toktobekov@gmail.com>
2024-09-11 12:57:30 +0600

    asset tests

---
+++ tests/asset.test.ts
@@ -0,0 +1,59 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program, web3, BN } from "@coral-xyz/anchor";
+import {
+  getAssociatedTokenAddressSync,
+  getAccount,
+  getMint,
+} from "@solana/spl-token";
+import { findPDA, getLogs, getVaultMintAddress } from "./helpers";
+import { Asset } from "../target/types/asset";
+import { expect, assert } from "chai";
+
+describe("asset", () => {
+  // Configure the client to use the local cluster.
+  const provider = anchor.AnchorProvider.env();
+  const program = anchor.workspace.Asset as Program<Asset>;
+  const user = provider.publicKey;
+  const conn = new web3.Connection(provider.connection.rpcEndpoint, {
+    commitment: "confirmed",
+  });
+
+  anchor.setProvider(provider);
+
+  it("init", async () => {
+    // Add your test here.
+    const tx = await program.methods.initialize().rpc({
+      commitment: "confirmed",
+    });
+
+    const logs = await getLogs(program, tx);
+    console.log(logs[10]);
+  });
+
+  it("Buy tokens", async () => {
+    const amount = 1_000_000; // is 1 token, as token::decimals = 6
+    const [vault, mint_address] = await getVaultMintAddress(program, user);
+
+    const vaultBalanceBefore = await conn.getBalance(vault);
+    await program.methods.buyToken(new BN(amount)).rpc({
+      commitment: "confirmed",
+    });
+    
+    // check balance
+    const vaultBalanceAfter = await conn.getBalance(vault);
+    expect(vaultBalanceAfter - vaultBalanceBefore).equals(1000*amount);
+
+    // check mint
+    const mintAccount = await getMint(conn, mint_address);
+    assert(mintAccount.decimals == 6, "Mint has wrong decimals");
+    assert(mintAccount.mintAuthority.equals(vault), "Wrong mint authority");
+    assert(mintAccount.supply == BigInt(amount), "Mint supply is invalid");
+
+    // check token
+    const ata_address = await getAssociatedTokenAddressSync(mint_address, user);
+    const ata = await getAccount(conn, ata_address);
+    assert(ata.mint.equals(mint_address), "Wrong ATA mint");
+    assert(ata.owner.equals(user), "ATA's owner is invalid.");
+    assert(ata.amount == BigInt(amount), "ATA's balance is invalid");
+  });
+});
---
+++ tests/helpers.ts
@@ -0,0 +1,33 @@
+import { web3, Program } from "@coral-xyz/anchor";
+import { Asset } from "../target/types/asset";
+
+const vaultSeedStr = "authVault"
+const mintSeedStr = "cullingToken";
+
+export const findPDA = (seeds: Array<Buffer>, programId: web3.PublicKey) =>
+  web3.PublicKey.findProgramAddressSync(seeds, programId);
+
+export const getVaultMintAddress = async (
+  program: Program<Asset>,
+  user: web3.PublicKey
+): Promise<[web3.PublicKey, web3.PublicKey]> => {
+  const vault_seeds = [Buffer.from(vaultSeedStr), user.toBuffer()];
+  const [vault_address] = findPDA(vault_seeds, program.programId);
+
+  // to get mint you need vault address
+  const mint_seeds = [Buffer.from(mintSeedStr), vault_address.toBuffer()];
+  const [mint_address] = findPDA(mint_seeds, program.programId);
+
+  return [vault_address, mint_address];
+};
+
+export const getLogs = async (
+  program: Program<Asset>,
+  tx: string
+): Promise<Array<string> | null> => {
+  const txDetails = await program.provider.connection.getTransaction(tx, {
+    maxSupportedTransactionVersion: 0,
+    commitment: "confirmed",
+  });
+  return txDetails?.meta?.logMessages || null;
+};
