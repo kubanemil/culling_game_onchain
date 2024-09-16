6aae70b1ea3ec7611c8166f6ddb435118218cecd
kubanemil <emil3toktobekov@gmail.com>
2024-09-16 14:02:44 +0600

    mint without metadata

--- programs/asset/src/instructions/buy_card.rs
+++ programs/asset/src/instructions/buy_card.rs
@@ -2,9 +2,6 @@ use crate::state::AuthVault;
 use anchor_lang::prelude::*;
 use anchor_spl::associated_token::AssociatedToken;
 use anchor_spl::token::{self, Token};
-use mpl_core::instructions::CreateV1CpiBuilder;
-use mpl_core::programs::MPL_CORE_ID;
-
 
 #[derive(Accounts)]
 #[instruction(card_id: u8)]
@@ -27,9 +24,6 @@ pub struct BuyCard<'info> {
     #[account(mut, associated_token::mint=mint, associated_token::authority=vault)]
     pub vault_ata: Account<'info, token::TokenAccount>,
 
-    /// CHECK: it's new program, no struct for it for now.
-    #[account(address=MPL_CORE_ID)]
-    pub mpl_core_program: UncheckedAccount<'info>,
     pub associated_token_program: Program<'info, AssociatedToken>,
     pub token_program: Program<'info, Token>,
     pub system_program: Program<'info, System>,
@@ -37,41 +31,20 @@ pub struct BuyCard<'info> {
 
 impl<'info> BuyCard<'info> {
     pub fn buy_card(&mut self, card_id: u8) -> Result<()> {
-        msg!("card ID: {}", card_id);
+        msg!("card ID & address: {} | {}", &card_id, self.card.key());
         let tokens_amount = 10 * 10u64.pow(6);
 
         // transfer tokens to vault
         let accounts = token::Transfer {
             from: self.signer_ata.to_account_info(),
             to: self.vault_ata.to_account_info(),
-            authority: self.vault.to_account_info(),
+            authority: self.signer.to_account_info(),
         };
 
-        let seeds = &[
-            "authVault".as_bytes(),
-            self.signer.key.as_ref(),
-            &[self.vault.bump],
-        ];
-        let signer_seeds = &[seeds.as_slice()];
-
-        let ctx = CpiContext::new_with_signer(
-            self.token_program.to_account_info(),
-            accounts,
-            signer_seeds,
-        );
+        let ctx = CpiContext::new(self.token_program.to_account_info(), accounts);
         token::transfer(ctx, tokens_amount)?;
 
-        // card is already created in validator.
-
-        // associate metadata with card using mpl_core
-        CreateV1CpiBuilder::new(&self.mpl_core_program.to_account_info())
-            .asset(&self.card.to_account_info())
-            .collection(None)
-            .authority(Some(&self.signer.to_account_info()))
-            .payer(&self.signer.to_account_info())
-            .name("Emil Token".to_owned())
-            .uri("https://developers.metaplex.com/core/create-asset".to_string())
-            .invoke()?;
+        // card account is already created in validator.
         Ok(())
     }
 }
--- tests/asset.test.ts
+++ tests/asset.test.ts
@@ -8,6 +8,7 @@ import {
 import { findPDA, getLogs, getVaultMintAddress } from "./helpers";
 import { Asset } from "../target/types/asset";
 import { expect, assert } from "chai";
+import { Keypair, PublicKey } from "@solana/web3.js";
 
 describe("asset", () => {
   // Configure the client to use the local cluster.
@@ -31,17 +32,18 @@ describe("asset", () => {
   });
 
   it("Buy tokens", async () => {
-    const amount = 1_000_000; // is 1 token, as token::decimals = 6
+    const amount = 20_000_000; // is 1 token, as token::decimals = 6
     const [vault, mint_address] = await getVaultMintAddress(program, user);
 
     const vaultBalanceBefore = await conn.getBalance(vault);
+    // invoke instruction
     await program.methods.buyToken(new BN(amount)).rpc({
       commitment: "confirmed",
     });
-    
+
     // check balance
     const vaultBalanceAfter = await conn.getBalance(vault);
-    expect(vaultBalanceAfter - vaultBalanceBefore).equals(1000*amount);
+    expect(vaultBalanceAfter - vaultBalanceBefore).equals(1000 * amount);
 
     // check mint
     const mintAccount = await getMint(conn, mint_address);
@@ -55,5 +57,26 @@ describe("asset", () => {
     assert(ata.mint.equals(mint_address), "Wrong ATA mint");
     assert(ata.owner.equals(user), "ATA's owner is invalid.");
     assert(ata.amount == BigInt(amount), "ATA's balance is invalid");
+  });
+
+  it("buy a card", async () => {
+    const cardId = 7;
+
+    const cardBuffer = Buffer.alloc(1);
+    cardBuffer.writeUInt8(cardId);
+    const [cardAddress] = findPDA(
+      [Buffer.from("card"), cardBuffer, user.toBuffer()],
+      program.programId
+    );
+    console.log("card address: ", cardAddress.toString());
+
+    const tx = await program.methods
+      .buyCard(cardId)
+      .accounts({
+        card: cardAddress,
+      })
+      .rpc();
+    const logs = await getLogs(program, tx);
+    console.log(logs);
   });
 });
