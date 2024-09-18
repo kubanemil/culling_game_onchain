5fd936be028f4595d46b7dab0202bff4dddc2c65
kubanemil <emil3toktobekov@gmail.com>
2024-09-18 17:37:36 +0600

    create metadata -> udpate metadata

--- programs/asset/src/instructions/set_metadata.rs
+++ programs/asset/src/instructions/set_metadata.rs
@@ -1,7 +1,9 @@
+use crate::state::AuthVault;
 use anchor_lang::prelude::*;
-use anchor_spl::metadata::mpl_token_metadata::instructions::CreateMetadataAccountV3CpiBuilder;
-use anchor_spl::metadata::mpl_token_metadata::types::DataV2;
-use anchor_spl::metadata::{Metadata, MetadataAccount};
+use anchor_lang::solana_program::sysvar;
+use anchor_spl::metadata::mpl_token_metadata::instructions::UpdateV1CpiBuilder;
+use anchor_spl::metadata::mpl_token_metadata::types::{Creator, Data};
+use anchor_spl::metadata::{Metadata as MetadataProgram, MetadataAccount};
 use anchor_spl::token::{self, Token};
 
 #[derive(Accounts)]
@@ -10,44 +12,51 @@ pub struct SetMetadata<'info> {
     #[account(mut)]
     pub signer: Signer<'info>,
 
-    #[account(seeds=[b"card", &[card_id][..], signer.key().as_ref()], bump)]
+    #[account(mut, seeds=[b"card", &[card_id][..], signer.key().as_ref()], bump)]
     pub card: Account<'info, token::Mint>,
 
-    /// CHECK: metadata account
-    #[account(seeds=[b"metadata", metadata_program.key().as_ref(), card.key().as_ref()], seeds::program=metadata_program.key(), bump)]
+    #[account(mut, seeds=[b"authVault", signer.key().as_ref()], bump=vault.bump)]
+    pub vault: Account<'info, AuthVault>,
+
+    #[account(mut)]
     pub metadata: Account<'info, MetadataAccount>,
 
-    pub metadata_program: Program<'info, Metadata>,
+    pub metadata_program: Program<'info, MetadataProgram>,
     pub token_program: Program<'info, Token>,
     pub system_program: Program<'info, System>,
+    /// CHECK: account constraints checked in account trait
+    #[account(address = sysvar::instructions::id())]
+    pub sysvar_instructions: UncheckedAccount<'info>,
 }
 
 impl<'info> SetMetadata<'info> {
-    pub fn set_metadata(&mut self, card_id: u8, name: String, uri: String) -> Result<()> {
+    pub fn set_metadata(&mut self, card_id: u8, new_name: String, new_uri: String) -> Result<()> {
         msg!("card ID & address: {} | {}", &card_id, self.card.key());
-        msg!("metadata address: {}", self.metadata.key());
-        let symbol = name.clone() + "@";
+        msg!("metadata address: {}", &self.metadata.key());
 
         // use metaplex_program to attach metadata to card
-        let card_data = DataV2 {
-            name,
-            symbol: symbol,
-            uri,
-            seller_fee_basis_points: 1,
-            creators: None,
-            collection: None,
-            uses: None,
+        let creators = Some(vec![Creator {
+            address: self.vault.key(),
+            verified: false,
+            share: 100,
+        }]);
+        let card_data = Data {
+            name: new_name,
+            symbol: self.metadata.symbol.clone(),
+            uri: new_uri,
+            seller_fee_basis_points: 10,
+            creators,
         };
 
-        CreateMetadataAccountV3CpiBuilder::new(&self.metadata_program.to_account_info())
+        UpdateV1CpiBuilder::new(&self.metadata_program)
             .metadata(&self.metadata.to_account_info())
-            .mint(&self.card.to_account_info())
-            .mint_authority(&self.signer.to_account_info())
-            .payer(&self.signer.to_account_info())
-            .update_authority(&self.signer.to_account_info(), false)
-            .system_program(&self.system_program.to_account_info())
             .data(card_data)
-            .is_mutable(true)
+            .mint(&self.card.to_account_info())
+            .authority(&self.signer.to_account_info())
+            .new_update_authority(self.vault.key())
+            .system_program(&self.system_program.to_account_info())
+            .sysvar_instructions(&self.sysvar_instructions.to_account_info())
+            .payer(&self.signer.to_account_info())
             .invoke()?;
         Ok(())
     }
--- tests/asset.test.ts
+++ tests/asset.test.ts
@@ -1,21 +1,11 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program, web3, BN } from "@coral-xyz/anchor";
+import { PublicKey } from "@solana/web3.js";
 import {
   getAssociatedTokenAddressSync,
   getAccount,
   getMint,
-  TOKEN_PROGRAM_ID,
 } from "@solana/spl-token";
-import { findPDA, getLogs, getVaultMintAddress } from "./helpers";
-import { Asset } from "../target/types/asset";
-import { expect, assert } from "chai";
-import { Keypair, PublicKey } from "@solana/web3.js";
-import {
-  createNft,
-  findMetadataPda,
-  mplTokenMetadata,
-} from "@metaplex-foundation/mpl-token-metadata";
-import NodeWallet from "@coral-xyz/anchor/dist/cjs/nodewallet";
+import * as anchor from "@coral-xyz/anchor";
+import { Program, web3, BN } from "@coral-xyz/anchor";
 import {
   percentAmount,
   keypairIdentity,
@@ -23,6 +13,18 @@ import {
   generateSigner,
 } from "@metaplex-foundation/umi";
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import {
+  findMetadataPda,
+  mplTokenMetadata,
+  createV1,
+  TokenStandard,
+  fetchDigitalAsset,
+} from "@metaplex-foundation/mpl-token-metadata";
+import { fromWeb3JsPublicKey } from "@metaplex-foundation/umi-web3js-adapters";
+import NodeWallet from "@coral-xyz/anchor/dist/cjs/nodewallet";
+import { expect, assert } from "chai";
+import { findPDA, getLogs, getVaultMintAddress } from "./helpers";
+import { Asset } from "../target/types/asset";
 
 const METAPLEX_PROGRAM_ID = new PublicKey(
   "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
@@ -61,23 +63,34 @@ describe("asset", async () => {
     ],
     program.programId
   );
+  const card = fromWeb3JsPublicKey(cardAddress);
 
   const vaultAta = await getAssociatedTokenAddressSync(
     mint_address,
     vault,
     true
   );
-  const card = generateSigner(umi);
 
   it("init", async () => {
     // Add your test here.
     const tx = await program.methods.initialize().rpc({
       commitment: "confirmed",
     });
-
     const logs = await getLogs(program, tx);
     console.log(logs[10]);
   });
+
+  // it("check mint and cards are created", async () => {
+  //   const [card1] = findPDA(
+  //     [Buffer.from("card"), Buffer.from(new Uint8Array([1])), user.toBuffer()],
+  //     program.programId
+  //   );
+
+  //   const cardInfo = await getMint(conn, card1);
+  //   assert(cardInfo.decimals == 0, "Card is not unit");
+  //   assert(cardInfo.mintAuthority.equals(user), "Wrong owner of the card");
+  //   assert(cardInfo.isInitialized == true, "Mint is not initialized");
+  // });
 
   it("Buy tokens", async () => {
     const amount = 20_000_000; // is 1 token, as token::decimals = 6
@@ -98,7 +111,7 @@ describe("asset", async () => {
     assert(mintAccount.mintAuthority.equals(vault), "Wrong mint authority");
     assert(mintAccount.supply == BigInt(amount), "Mint supply is invalid");
 
-    // check token
+    // check ata
     const ata_address = await getAssociatedTokenAddressSync(mint_address, user);
     const ata = await getAccount(conn, ata_address);
     assert(ata.mint.equals(mint_address), "Wrong ATA mint");
@@ -126,64 +139,80 @@ describe("asset", async () => {
       "vault did not receive full amount"
     );
 
-    // check card token
-    let tokenAccounts = await conn.getTokenAccountsByOwner(user, {
-      programId: TOKEN_PROGRAM_ID,
-    });
-    for (let account of tokenAccounts.value) {
-      let accountInfo = account.account.data;
-      console.log(
-        `Token Mint address/owner: ${account.pubkey} ${account.account.owner} ${account.account.lamports}`
-      );
-      console.log(cardAddress.toString(), mint_address.toString());
-      // assert(account.pubkey == cardAddress, "Wrong card pubkey")
-    }
-
+    // check card
     const cardInfo = await getMint(conn, cardAddress);
     assert(cardInfo.decimals == 0, "Card is not unit");
     assert(cardInfo.mintAuthority.equals(user), "Wrong owner of the card");
     assert(cardInfo.isInitialized == true, "Mint is not initialized");
+
+    // check ata
+    const ata_address = await getAssociatedTokenAddressSync(cardAddress, user);
+    const ata = await getAccount(conn, ata_address);
+    assert(ata.mint.equals(cardAddress), "Wrong ATA mint");
+    assert(ata.owner.equals(user), "ATA's owner is invalid.");
+    assert(ata.amount == BigInt(1), "ATA's balance is invalid");
   });
 
   it("init metadata", async () => {
-    await createNft(umi, {
-      mint: card,
-      name: "cullingMetaToken",
-      symbol: "cM@",
-      uri: "https://arweave.net/culling",
-      sellerFeeBasisPoints: percentAmount(5),
-      creators: null,
-      collectionDetails: {
-        __kind: "V1",
-        size: 10,
-      },
-    }).sendAndConfirm(umi);
-  });
-
-  it("set metadata", async () => {
-    const cardId = 7;
     const name = "cullingMetaToken";
     const uri = "https://mail.google.com/mail/u/0/";
 
-    // const [cardMetadataAddress] = findPDA(
-    //   [
-    //     Buffer.from("metadata"),
-    //     METAPLEX_PROGRAM_ID.toBuffer(),
-    //     cardAddress.toBuffer(),
-    //   ],
-    //   METAPLEX_PROGRAM_ID
-    // );
-    const cardMetadataAddress = findMetadataPda(umi, {
-      mint: cardAddress.toString(),
+    const tx = await createV1(umi, {
+      mint: card,
+      authority: creator,
+      name: name,
+      uri: uri,
+      sellerFeeBasisPoints: percentAmount(0),
+      tokenStandard: TokenStandard.FungibleAsset,
+    }).sendAndConfirm(umi);
+    console.log("card metadata is created.");
+
+    const cardMetadata = await fetchDigitalAsset(umi, card);
+
+    console.log("Metadata address: ", cardMetadata.publicKey);
+    expect(cardMetadata.metadata.uri).eq(uri, "Invalid URI");
+    expect(cardMetadata.metadata.name).eq(name, "Invalid card name");
+    expect(cardMetadata.metadata.updateAuthority.toString()).eq(
+      user.toString(),
+      "Invalid update authority"
+    );
+    expect(cardMetadata.metadata.isMutable).eq(
+      true,
+      "card metadata is immutable"
+    );
+    expect(cardMetadata.mint.publicKey).eq(card, "Invalid card address");
+  });
+
+  it("update metadata", async () => {
+    const [cardMetadataAddress] = findMetadataPda(umi, {
+      mint: card,
     });
 
+    const new_name = "newCullingMetaToken";
+    const new_uri = "rickrool_URL";
+
     const tx = await program.methods
-      .setMetadata(cardId, name, uri)
+      .setMetadata(cardId, new_name, new_uri)
       .accounts({
         signer: user,
         card: cardAddress,
         metadata: cardMetadataAddress,
       })
       .rpc();
+    await conn.confirmTransaction(tx, "confirmed");
+
+    const cardMetadata = await fetchDigitalAsset(umi, card);
+
+    expect(cardMetadata.metadata.uri).eq(new_uri, "Invalid URI");
+    expect(cardMetadata.metadata.name).eq(new_name, "Invalid card name");
+    expect(cardMetadata.mint.publicKey).eq(card, "Invalid card address");
+    expect(cardMetadata.metadata.updateAuthority.toString()).eq(
+      vault.toString(),
+      "Wrong update authority"
+    );
+    expect(cardMetadata.metadata.isMutable).eq(
+      true,
+      "Card metadata is immutable"
+    );
   });
 });
