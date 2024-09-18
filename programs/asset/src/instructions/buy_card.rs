ba62d5971f59a37b5807d1efa4a41f00b4231368
kubanemil <emil3toktobekov@gmail.com>
2024-09-18 17:21:57 +0600

    init card -> mint card

--- programs/asset/src/instructions/buy_card.rs
+++ programs/asset/src/instructions/buy_card.rs
@@ -1,3 +1,4 @@
+use crate::error::ErrorCode;
 use crate::state::AuthVault;
 use anchor_lang::prelude::*;
 use anchor_spl::associated_token::AssociatedToken;
@@ -12,11 +13,14 @@ pub struct BuyCard<'info> {
     #[account(mut, seeds=[b"cullingToken", vault.key().as_ref()], bump=vault.mint_bump)]
     pub mint: Account<'info, token::Mint>,
 
-    #[account(init, payer=signer, seeds=[b"card", &[card_id][..], signer.key().as_ref()], bump, mint::decimals=0, mint::authority=signer)]
+    #[account(init_if_needed, seeds=[b"card", &[card_id][..], vault.owner.as_ref()], bump, payer=signer, mint::decimals=0, mint::authority=vault.owner)]
     pub card: Account<'info, token::Mint>,
 
     #[account(init_if_needed, payer=signer, associated_token::mint=mint, associated_token::authority=signer)]
-    pub signer_ata: Account<'info, token::TokenAccount>,
+    pub signer_mint_ata: Account<'info, token::TokenAccount>,
+
+    #[account(init_if_needed, payer=signer, associated_token::mint=card, associated_token::authority=signer)]
+    pub signer_card_ata: Account<'info, token::TokenAccount>,
 
     #[account(mut, seeds=[b"authVault", signer.key().as_ref()], bump=vault.bump)]
     pub vault: Account<'info, AuthVault>,
@@ -32,19 +36,28 @@ pub struct BuyCard<'info> {
 impl<'info> BuyCard<'info> {
     pub fn buy_card(&mut self, card_id: u8) -> Result<()> {
         msg!("card ID & address: {} | {}", &card_id, self.card.key());
+        require!(card_id < 10, ErrorCode::InvalidCardId);
+
         let tokens_amount = 10 * 10u64.pow(6); // 10 cullingTokens
 
         // transfer tokens to vault
-        let accounts = token::Transfer {
-            from: self.signer_ata.to_account_info(),
+        let transfer_accounts = token::Transfer {
+            from: self.signer_mint_ata.to_account_info(),
             to: self.vault_ata.to_account_info(),
             authority: self.signer.to_account_info(),
         };
 
-        let ctx = CpiContext::new(self.token_program.to_account_info(), accounts);
+        let ctx = CpiContext::new(self.token_program.to_account_info(), transfer_accounts);
         token::transfer(ctx, tokens_amount)?;
 
-        // card account is already created in validator.
-        Ok(())
+        // mint card
+        let mint_accounts = token::MintTo {
+            mint: self.card.to_account_info(),
+            to: self.signer_card_ata.to_account_info(),
+            authority: self.signer.to_account_info(),
+        };
+
+        let ctx = CpiContext::new(self.token_program.to_account_info(), mint_accounts);
+        token::mint_to(ctx, 1)
     }
 }
