054f7135f1d405e92c35db94b633a1acab27e3e2
kubanemil <emil3toktobekov@gmail.com>
2024-09-24 11:51:50 +0600

    vote() ix

---
+++ programs/governance/src/constants.rs
@@ -0,0 +1,5 @@
+use anchor_lang::prelude::*;
+
+#[constant]
+pub const ASSET_PROGRAM_ID: Pubkey = pubkey!("BwDuywJ5NxriMUEJ7EMKFth7joH3W2snbMfQtKp4nwrf");
+pub const VAULT_ADDRESS: Pubkey = pubkey!("BwDuywJ5NxriMUEJ7EMKFth7joH3W2snbMfQtKp4nwrf");
--- programs/governance/src/instructions/vote.rs
+++ programs/governance/src/instructions/vote.rs
@@ -1,6 +1,10 @@
-use anchor_lang::prelude::*;
-
+use crate::constants::{ASSET_PROGRAM_ID, VAULT_ADDRESS};
 use crate::state::Vote;
+use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken as AssociatedTokenProgram,
+    token::{self, Token as TokenProgram, TokenAccount},
+};
 
 #[derive(Accounts)]
 #[instruction(amendment_id: u32)]
@@ -12,6 +16,22 @@ pub struct VoteFor<'info> {
         seeds=[b"vote", signer.key().as_ref(), &amendment_id.to_le_bytes()[..]], bump)]
     pub vote: Account<'info, Vote>,
 
+    #[account(mut, seeds=[b"cullingToken", VAULT_ADDRESS.as_ref()], bump)]
+    pub mint: Account<'info, token::Mint>,
+
+    #[account(init_if_needed, payer=signer,
+        associated_token::mint=mint, associated_token::authority=vote)]
+    pub vote_ata: Account<'info, token::TokenAccount>,
+
+    #[account(init_if_needed, payer=signer,
+        associated_token::mint=mint, associated_token::authority=signer)]
+    pub signer_ata: Account<'info, TokenAccount>,
+
+    /// CHECK: Our Asset program to get game tokens
+    #[account(address=ASSET_PROGRAM_ID)]
+    pub asset_program: UncheckedAccount<'info>,
+    pub associated_token_program: Program<'info, AssociatedTokenProgram>,
+    pub token_program: Program<'info, TokenProgram>,
     pub system_program: Program<'info, System>,
 }
 
@@ -19,7 +39,7 @@ impl<'info> VoteFor<'info> {
     pub fn vote_for(&mut self, amendment_id: u32, accept: bool, tokens: u64) -> Result<()> {
         msg!("Voting for amendment with id: {:?}", amendment_id);
 
-        todo!("transfer tokens to vote pda");
+        self.transfer_tokens(tokens)?;
 
         self.vote.set_inner(Vote {
             voter: self.signer.key(),
@@ -28,6 +48,19 @@ impl<'info> VoteFor<'info> {
             tokens,
         });
 
+        Ok(())
+    }
+
+    pub fn transfer_tokens(&mut self, amount: u64) -> Result<()> {
+        // transfer tokens to vote
+        let transfer_accounts = token::Transfer {
+            from: self.signer_ata.to_account_info(),
+            to: self.vote_ata.to_account_info(),
+            authority: self.signer.to_account_info(),
+        };
+
+        let ctx = CpiContext::new(self.token_program.to_account_info(), transfer_accounts);
+        token::transfer(ctx, amount)?;
         Ok(())
     }
 }
